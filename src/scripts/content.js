ca26b091ac9567ef184f0028221832f472821700
Timothy Kempf <tim@kemp59f.info>
2014-07-04 22:13:00 -0700

    domquery just isn't quite ready yet

--- src/scripts/content.js
+++ src/scripts/content.js
@@ -1,35 +1,38 @@
 require('../styles/content.less');
 var gifjs = require('gif.js');
-var dq = require('domquery');
+var $ = require('jquery');
 var gifit_button_template = require('../templates/button.hbs');
 var gifit_overlay_template = require('../templates/overlay.hbs');
 var gifit_options_template = require('../templates/options.hbs');
 
 const MAXIMUM_Z_INDEX = 2147483647;
 
-var body = dq('body');
-var youtube_video_container = dq('#player-api .html5-video-container');
-var youtube_video = dq('#player-api video.video-stream');
-var youtube_controls = dq('#player-api .html5-video-controls .html5-player-chrome');
-var gifit_button = dq( gifit_button_template() );
-var gifit_canvas = dq('<canvas></canvas>');
-var gifit_canvas_context = gifit_canvas[0].getContext('2d');
-var gifit_overlay = dq( gifit_overlay_template() );
-var gifit_close = gifit_overlay.select('#gifit-close');
-var gifit_options = dq( gifit_options_template() );
+// get DOM selections sorted
+var $body = $('body');
+var $youtube_video_container = $('#player-api .html5-video-container');
+var $youtube_video = $('#player-api video.video-stream');
+var youtube_video = $youtube_video.get(0);
+var $youtube_controls = $('#player-api .html5-video-controls .html5-player-chrome');
+var $gifit_button = $( gifit_button_template() );
+var $gifit_canvas = $('<canvas></canvas>');
+var gifit_canvas_context = $gifit_canvas.get(0).getContext('2d');
+var $gifit_overlay = $( gifit_overlay_template() );
+var $gifit_close = $gifit_overlay.find('#gifit-close');
+var $gifit_options = $( gifit_options_template() );
 
-youtube_controls.add( gifit_button );
-body.add( gifit_canvas );
-body.add( gifit_overlay );
-body.add( gifit_options );
+$youtube_controls.append( $gifit_button );
+$body.append( $gifit_canvas );
+$body.append( $gifit_overlay );
+$body.append( $gifit_options );
 
 var gif;
 var capture_interval;
 
 var startCapture = function( options ){
     if( options.width ){
-        gifit_canvas.width = options.width || 320;
-        gifit_canvas.height = ( gifit_canvas.width * parseInt( youtube_video.style.height, 10 ) ) / parseInt( youtube_video.style.width, 10 );
+        $gifit_canvas
+            .width( options.width || 320 )
+            .height( $gifit_canvas.width() * parseInt( $youtube_video.height(), 10 ) ) / parseInt( $youtube_video.width(), 10 );
     }
     var frame_delay = 1000 / ( options.framerate || 10 );
     gif = new gifjs.GIF({
@@ -42,8 +45,8 @@ var startCapture = function( options ){
         window.open( URL.createObjectURL( blob ) );
     });
     capture_interval = setInterval( function(){
-        gifit_context.drawImage( youtube_video, 0, 0, gifit_canvas.width, gifit_canvas.height );
-        gif.addFrame( gifit_canvas, {
+        gifit_canvas_context.drawImage( $youtube_video.get(0), 0, 0, $gifit_canvas.width(), $gifit_canvas.height() );
+        gif.addFrame( $gifit_canvas.get(0), {
             delay: frame_delay,
             copy: true
         });
@@ -61,24 +64,13 @@ var endCapture = function(){
     }
 };
 
-gifit_button.on( 'click', function( e ){
+$gifit_button.on( 'click', function( e ){
     console.log('gifit button clicked', arguments);
-    youtube_video[0].pause();
-    body.addClass('gifit-active');
+    youtube_video.pause();
+    $body.addClass('gifit-active');
 });
 
-gifit_close.on( 'click', function( e ){
+$gifit_close.on( 'click', function( e ){
     e.preventDefault();
-    body.removeClass('gifit-active');
-});
-
-chrome.runtime.onMessage.addListener( function( request, sender, cb ){
-    switch( request.action ){
-        case 'record':
-            startCapture( request.options );
-        break;
-        case 'stop':
-            endCapture();
-        break;
-    }
+    $body.removeClass('gifit-active');
 });
