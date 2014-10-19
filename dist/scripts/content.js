6fb2d96581222ddbcd5d34383e52df2b66361f29
Timothy Kempf <the.kaempfer@gmail.com>
2014-10-19 15:14:23 -0700

    fix indentation

--- dist/scripts/content.js
+++ dist/scripts/content.js
@@ -14329,109 +14329,109 @@ var gif;
 var capture_interval;
 
 $.Velocity.RegisterEffect('gifit.slideUpIn', {
-    defaultDuration: 900,
-    calls: [ 
-        [{
-            //opacity: [ 1, 0 ],
-            translateY: [ 0, 400 ],
-            translateZ: 0
-        }, 1, {
-            easing: [0.165, 0.84, 0.44, 1]
-        }]
-    ]
+	defaultDuration: 900,
+	calls: [ 
+		[{
+			//opacity: [ 1, 0 ],
+			translateY: [ 0, 400 ],
+			translateZ: 0
+		}, 1, {
+			easing: [0.165, 0.84, 0.44, 1]
+		}]
+	]
 });
 
 $.Velocity.RegisterEffect('gifit.slideDownOut', {
-    defaultDuration: 500,
-    calls: [ 
-        [{
-            //opacity: [ 0, 1 ],
-            translateY: 400,
-            translateZ: 0
-        }, 1, {
-            easing: [0.895, 0.03, 0.685, 0.22]
-        }]
-    ],
-    reset: { translateY: 0 }
+	defaultDuration: 500,
+	calls: [ 
+		[{
+			//opacity: [ 0, 1 ],
+			translateY: 400,
+			translateZ: 0
+		}, 1, {
+			easing: [0.895, 0.03, 0.685, 0.22]
+		}]
+	],
+	reset: { translateY: 0 }
 });
 
 var generateGIF = function( options ){
-    $gifit_options_form.find('input, button').prop( 'disabled', true );
-    $gifit_options.addClass('processing');
-    // generate GIF options
-    var defaults = {
-        width: 320,
-        colors: 128,
-        framerate: 10,
-        quality: 5
-    };
-    options = $.extend( defaults, options );
-    options.frame_interval = 1000 / options.framerate;
-    options.start = toSeconds( options.start );
-    options.end = toSeconds( options.end );
-    options.height = Math.ceil( ( options.width * $youtube_video.height() ) / $youtube_video.width() );
-    // create GIF encoder
-    gif = new gifjs.GIF({
-        workers: 8,
-        quality: options.quality,
-        repeat: 0,
-        width: options.width,
-        height: options.height,
-        workerScript: chrome.runtime.getURL('scripts/vendor/gif.worker.js')
-    });
-    gif.on( 'finished', function( blob ){
-        $gifit_options_form.find('input, button').prop( 'disabled', false );
-        $gifit_options.removeClass('processing');
-        window.open( URL.createObjectURL( blob ) );
-    });
-    // make sure the video is paused before we jump frames
-    if( !youtube_video.paused ){
-        youtube_video.pause();
-    }
-    // prepare canvas for receiving frames
-    $gifit_canvas
-        .attr( 'width', options.width )
-        .attr( 'height', options.height );
-    // play the part of the video we want to convert
-    youtube_video.currentTime = options.start;
-    youtube_video.play();
-    var addFrameInterval = setInterval( function(){
-        if( youtube_video.currentTime >= options.end ){
-            // render the GIF
-            gif.render();
-            youtube_video.pause();
-            clearInterval( addFrameInterval );
-            return;
-        }
-        gifit_canvas_context.drawImage( youtube_video, 0, 0, options.width, options.height );
-        gif.addFrame( $gifit_canvas.get(0), {
-            delay: options.frame_interval,
-            copy: true
-        });
-    }, options.frame_interval );
+	$gifit_options_form.find('input, button').prop( 'disabled', true );
+	$gifit_options.addClass('processing');
+	// generate GIF options
+	var defaults = {
+		width: 320,
+		colors: 128,
+		framerate: 10,
+		quality: 5
+	};
+	options = $.extend( defaults, options );
+	options.frame_interval = 1000 / options.framerate;
+	options.start = toSeconds( options.start );
+	options.end = toSeconds( options.end );
+	options.height = Math.ceil( ( options.width * $youtube_video.height() ) / $youtube_video.width() );
+	// create GIF encoder
+	gif = new gifjs.GIF({
+		workers: 8,
+		quality: options.quality,
+		repeat: 0,
+		width: options.width,
+		height: options.height,
+		workerScript: chrome.runtime.getURL('scripts/vendor/gif.worker.js')
+	});
+	gif.on( 'finished', function( blob ){
+		$gifit_options_form.find('input, button').prop( 'disabled', false );
+		$gifit_options.removeClass('processing');
+		window.open( URL.createObjectURL( blob ) );
+	});
+	// make sure the video is paused before we jump frames
+	if( !youtube_video.paused ){
+		youtube_video.pause();
+	}
+	// prepare canvas for receiving frames
+	$gifit_canvas
+		.attr( 'width', options.width )
+		.attr( 'height', options.height );
+	// play the part of the video we want to convert
+	youtube_video.currentTime = options.start;
+	youtube_video.play();
+	var addFrameInterval = setInterval( function(){
+		if( youtube_video.currentTime >= options.end ){
+			// render the GIF
+			gif.render();
+			youtube_video.pause();
+			clearInterval( addFrameInterval );
+			return;
+		}
+		gifit_canvas_context.drawImage( youtube_video, 0, 0, options.width, options.height );
+		gif.addFrame( $gifit_canvas.get(0), {
+			delay: options.frame_interval,
+			copy: true
+		});
+	}, options.frame_interval );
 };
 
 $gifit_button.on( 'click', function( e ){
-    $body.toggleClass('gifit-active');
-    if( $gifit_options.is(':visible') ){
-        $gifit_options.velocity( 'gifit.slideDownOut', 250, {
-            complete: function(){
-                $gifit_options.find('fieldset, .gifit-actions').velocity({
-                    opacity: 0
-                }, 0 );
-            }
-        });
-    }
-    else {
-        $gifit_options.velocity( 'gifit.slideUpIn', 250 );
-        $gifit_options.find('fieldset, .gifit-actions').velocity( 'transition.slideUpIn', { stagger: 35 }, 55 );
-    }
+	$body.toggleClass('gifit-active');
+	if( $gifit_options.is(':visible') ){
+		$gifit_options.velocity( 'gifit.slideDownOut', 250, {
+			complete: function(){
+				$gifit_options.find('fieldset, .gifit-actions').velocity({
+					opacity: 0
+				}, 0 );
+			}
+		});
+	}
+	else {
+		$gifit_options.velocity( 'gifit.slideUpIn', 250 );
+		$gifit_options.find('fieldset, .gifit-actions').velocity( 'transition.slideUpIn', { stagger: 35 }, 55 );
+	}
 });
 
 $gifit_options_form.on( 'submit', function( e ){
-    e.preventDefault();
-    var options = getFormData( $gifit_options_form.get(0) );
-    generateGIF( options );
+	e.preventDefault();
+	var options = getFormData( $gifit_options_form.get(0) );
+	generateGIF( options );
 });
 
 },{"../styles/content.less":18,"../templates/button.hbs":19,"../templates/options.hbs":20,"./vendor/getFormData.js":16,"./vendor/toSeconds.js":17,"gif.js":1,"jquery":10,"velocity-animate":13,"velocity-animate/velocity.ui.js":14}],16:[function(require,module,exports){
